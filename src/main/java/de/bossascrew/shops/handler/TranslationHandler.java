29cc1d31bcb0e8567cec9c90d40909de2f1e74a9
CubBossa <leonard.bausenwein@web.de>
2021-11-13 16:41:13 +0100

    generate translation file

--- src/main/java/de/bossascrew/shops/handler/TranslationHandler.java
+++ src/main/java/de/bossascrew/shops/handler/TranslationHandler.java
@@ -1,12 +1,15 @@
 package de.bossascrew.shops.handler;
 
+import de.bossascrew.shops.ShopPlugin;
+import de.bossascrew.shops.util.LoggingPolicy;
 import lombok.Getter;
+import org.bukkit.configuration.file.YamlConfiguration;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
 public class TranslationHandler {
-
 
 	@Getter
 	private static TranslationHandler instance;
@@ -24,8 +27,23 @@ public class TranslationHandler {
 		activeLanguage = languageKey;
 		messageFormats.clear();
 
+		File file = new File(ShopPlugin.getInstance().getDataFolder(), "lang/" + languageKey + ".yml");
+		if (!file.exists()) {
+			ShopPlugin.getInstance().saveResource("lang/" + languageKey + ".yml", false);
+			file = new File(ShopPlugin.getInstance().getDataFolder(), "lang/" + languageKey + ".yml");
+			if (!file.exists()) {
 
-
+				ShopPlugin.getInstance().log(LoggingPolicy.ERROR, "Error while creating language.yml for " + languageKey);
+				return;
+			}
+		}
+		YamlConfiguration cfg = YamlConfiguration.loadConfiguration(file);
+		Map<String, Object> map = cfg.getValues(true);
+		for (Map.Entry<String, Object> entry : map.entrySet()) {
+			if (entry.getValue() instanceof String s) {
+				messageFormats.put(entry.getKey(), s);
+			}
+		}
 	}
 
 	public String getMessage(String key) {
